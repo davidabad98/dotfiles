09680261743e3947a92a7407cd08f6b78d4956f2
David Abad <david@corp.paymentevolution.com>
2025-11-07 16:54:01 -0500

    Added comment.lua, neotree improvement, quickfix shortcuts

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,8 +7,21 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
-	lazy = false, -- neo-tree will lazily load itself
-	config = function()
-		vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left<CR>", {})
-	end,
+	lazy = false,
+	keys = {
+		{ "<C-e>", "<cmd>Neotree filesystem reveal left<CR>", desc = "Neo-tree toggle/reveal" },
+	},
+	opts = {
+		-- optional: if Neo-tree is the last window, close Neovim
+		close_if_last_window = true,
+		event_handlers = {
+			{
+				event = "file_opened",
+				handler = function(_)
+					-- close the tree when a file is opened
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			},
+		},
+	},
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,34 +1,34 @@
 -- ========== Basic Settings ==========
-vim.opt.number = true          -- Show line numbers
-vim.opt.relativenumber = true  -- Relative numbers for quick jumps
-vim.opt.tabstop = 2            -- Tab = 4 spaces
-vim.opt.shiftwidth = 2         -- Indentation = 2 spaces
-vim.opt.expandtab = true       -- Convert tabs to spaces
-vim.opt.smartindent = true     -- Auto-indent new lines
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Relative numbers for quick jumps
+vim.opt.tabstop = 2 -- Tab = 4 spaces
+vim.opt.shiftwidth = 2 -- Indentation = 2 spaces
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.smartindent = true -- Auto-indent new lines
 
-vim.opt.wrap = false           -- Don’t wrap long lines
-vim.opt.cursorline = true      -- Highlight current line
-vim.opt.scrolloff = 8          -- Keep cursor away from edges
-vim.opt.signcolumn = "yes"     -- Always show sign column
+vim.opt.wrap = false -- Don’t wrap long lines
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.scrolloff = 8 -- Keep cursor away from edges
+vim.opt.signcolumn = "yes" -- Always show sign column
 
 -- Always save files with Unix line endings (global)
-vim.o.fileformat  = "unix"
+vim.o.fileformat = "unix"
 vim.o.fileformats = "unix,dos"
 
 -- ========== Search ==========
-vim.opt.ignorecase = true      -- Case-insensitive search...
-vim.opt.smartcase = true       -- ...unless capital letters in query
-vim.opt.hlsearch = true        -- Highlight search results
-vim.opt.incsearch = true       -- Show matches while typing
+vim.opt.ignorecase = true -- Case-insensitive search...
+vim.opt.smartcase = true -- ...unless capital letters in query
+vim.opt.hlsearch = true -- Highlight search results
+vim.opt.incsearch = true -- Show matches while typing
 
 -- ========== Clipboard ==========
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
 
 -- ========== UI ==========
-vim.opt.termguicolors = true   -- Enable 24-bit colors
-vim.opt.splitbelow = true      -- Split below
-vim.opt.splitright = true      -- Split right
-vim.opt.colorcolumn = '88'     -- Enable vertical ruler line
+vim.opt.termguicolors = true -- Enable 24-bit colors
+vim.opt.splitbelow = true -- Split below
+vim.opt.splitright = true -- Split right
+vim.opt.colorcolumn = "88" -- Enable vertical ruler line
 
 -- ========== Keymaps ==========
 
@@ -55,16 +55,41 @@ vim.keymap.set("n", "<leader>j", "<C-w>j")
 vim.keymap.set("n", "<leader>k", "<C-w>k")
 vim.keymap.set("n", "<leader>l", "<C-w>l")
 
+-- ========== Quickfix ==========
+vim.keymap.set("n", "<C-j>", ":cnext<CR>", { desc = "Quickfix Next" })
+vim.keymap.set("n", "<C-k>", ":cprev<CR>", { desc = "Quickfix Prev" })
+
+-- helpers to toggle qf/loclist windows
+local function is_open(kind)
+	for _, win in ipairs(vim.fn.getwininfo()) do
+		if kind == "qf" and win.quickfix == 1 then
+			return true
+		end
+		if kind == "loc" and win.loclist == 1 then
+			return true
+		end
+	end
+	return false
+end
+
+local function toggle_qf()
+	if is_open("qf") then
+		vim.cmd.cclose()
+	else
+		vim.cmd.copen()
+	end
+end
+vim.keymap.set("n", "<leader>cq", toggle_qf, { desc = "Quickfix Toggle" })
 
 -- ========== Developer Essentials ==========
 
 -- keep half-page moves centered
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 
 -- keep search jumps centered and unfolded
-vim.keymap.set('n', 'n', 'nzzzv', { noremap = true, silent = true })
-vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
 
 -- Stay in visual mode while indenting
 vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
@@ -83,18 +108,18 @@ vim.opt.autoread = true
 vim.cmd("au FocusGained,BufEnter * checktime")
 
 -- Visual mode: paste over selection without overwriting the unnamed register
-vim.keymap.set('x', '<leader>p', '"_dP', { noremap = true, silent = true })
+vim.keymap.set("x", "<leader>p", '"_dP', { noremap = true, silent = true })
 
 -- Always normalize to LF line endings
-vim.api.nvim_create_autocmd({ "BufRead", "BufWritePre" }, {
-  pattern = "*",
-  command = "set ff=unix",
-})
+-- vim.api.nvim_create_autocmd({ "BufRead", "BufWritePre" }, {
+--   pattern = "*",
+--   command = "set ff=unix",
+-- })
 
 -- ========== Diagnostics ==========
 vim.o.updatetime = 250 -- 0.25 seconds idle time before CursorHold triggers
 vim.api.nvim_create_autocmd("CursorHold", {
-  callback = function()
-    vim.diagnostic.open_float(nil, { focus = false })
-  end,
+	callback = function()
+		vim.diagnostic.open_float(nil, { focus = false })
+	end,
 })
