1cf4464d08a41a2c7197e4b72248f4d4fa6b0493
David Abad <davidabad98@hotmail.com>
2026-04-13 15:08:13 -0500

    fix: treesitter config changed again

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.config").setup({
+			require("nvim-treesitter.configs").setup({
 				auto_install = true,
 				--ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "python",
 				--            "c_sharp", "razor", "html", "sql", "json", "markdown",
